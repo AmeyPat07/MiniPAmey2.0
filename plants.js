6b86dd04713412227dc51ccee6d245253040abfc
AmeyPat07 <ameypat07@gmail.com>
2025-10-11 21:54:32 +0530

    update project files

---
+++ plants.js
@@ -0,0 +1,17 @@
+const plants = [
+  { "plant": "Tomato", "link": "https://youtu.be/eySTo2GgvoY?si=Rx25OV1zMmIJgnfz" },
+  { "plant": "Basil", "link": "https://youtu.be/wmTrjYbuycE?si=n15E27n9j06oPn-5" },
+  { "plant": "Rose", "link": "https://youtu.be/WACx0f1fiiY?si=UOXJENvpb1lYjsLI" },
+  { "plant": "Lettuce", "link": "https://youtu.be/wj6hN9oQ4d4?si=hy1WZC0s7SmHMOVE" },
+  { "plant": "Spinach", "link": "https://youtu.be/gKi4Ls3XMYo?si=itNiMjqHfq4vF8p6" },
+  { "plant": "Mint", "link": "https://youtu.be/JRAJ3l2Mm4k?si=SFTXTQIF4De8wX6l" },
+  { "plant": "Marigold", "link": "https://youtu.be/KeqXc5rGVV4?si=RiIZqfJdWDaol1Rh" },
+  { "plant": "Coriander", "link": "https://youtu.be/ES1WuEE_xAY?si=r_UiTTStztbHmT_N" },
+  { "plant": "Chili Pepper", "link": "https://youtu.be/UgyjnGC8N4w?si=e8VBUsOW91RbYU_d" },
+  { "plant": "Radish", "link": "https://youtu.be/PcD-LHVLn-Q?si=gK6LJqC6o7--pufV" },
+  { "plant": "Sunflower", "link": "https://youtu.be/0FgNWD0upTc?si=odba-Z-qrQ13EZYW" },
+  { "plant": "Green Beans", "link": "https://youtu.be/lwUGrL92HQo?si=2psePQO9992bVosS" },
+  { "plant": "Okra", "link": "https://youtu.be/RU7cBD14iQ8?si=-z_D-GCJVrxMZaRM" },
+  { "plant": "Cucumber", "link": "https://youtu.be/Gj3Q9JBXw0Y?si=q73BK8-VEenAnXU1" },
+  { "plant": "Nasturtium", "link": "https://youtu.be/cHVp-PHnG8o?si=rywRPTCwRc6xiMkA" }
+];
